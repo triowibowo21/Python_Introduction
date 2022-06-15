d65ba1eeb45921a0736ab564cace018950327d3e
Trio Wibowo <triog74130021@gmail.com>
2022-06-15 08:14:44 +0700

    variabel & type

---
+++ 001_variabel_type.py
@@ -0,0 +1,18 @@
+panjang = 1.52
+lebar = 52.5
+bulan = 1
+hasil = lebar/panjang ** 2
+x = "contoh pertama"
+y = 'contoh ke dua'
+z = True
+
+
+print('panjang =', panjang)
+print('lebar = ', lebar)
+print('hasil = ', hasil)
+ 
+print('tipe variabel hasil adalah',type(hasil))
+print('tipe variabel bulan adalah',type(bulan))
+print('tipe variabel x adalah',type(x))
+print('tipe variabel y adalah',type(y))
+print('tipe variabel z adalah',type(z))
