32fd8a39954d7d842ba474ca12c6edc5d9d47104
Trio Wibowo <triog74130021@gmail.com>
2022-06-21 05:53:49 +0700

    instalasi package

---
+++ script.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+keluarga = ['irin', 4.23, 'agus', 4.69, 'sena', 4.36, 'dian', 4.81]
+tam_kel = keluarga + ['abi', 4.69]
+
+print(str(len(tam_kel)) + " elmen didalam list tamb_kel")
+
+np_kel = np.array(tam_kel)
